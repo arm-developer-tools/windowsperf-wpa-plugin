6619578ffcd67ed2b647b95126ee5dcd390f598b
Alaaeddine Chakroun <chakrounalaaeddine@gmail.com>
2023-10-17 15:47:25 +0000

    feat: add boilerplate files
    See merge request Linaro/WindowsPerf/wpa-plugin!2
    
    === ChangeLog ===
    
    * feat: add TableBuilder
    
    * chore: add missing DataOutput attribute in the data cooker
    
    * feat: add data cooker
    
    * feat: add source parser
    
    * feat: add SourceParser and CountingEvent
    
    * chore: add processing source

---
+++ WPAPlugin/WperfDataCooker.cs
@@ -0,0 +1,75 @@
+﻿// BSD 3-Clause License
+//
+// Copyright (c) 2022, Arm Limited
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this
+//    list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its
+//    contributors may be used to endorse or promote products derived from
+//    this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+using Microsoft.Performance.SDK;
+using Microsoft.Performance.SDK.Extensibility;
+using Microsoft.Performance.SDK.Extensibility.DataCooking;
+using Microsoft.Performance.SDK.Extensibility.DataCooking.SourceDataCooking;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading;
+using WperfWPAPlugin.Constants;
+using WperfWPAPlugin.Events;
+
+namespace WperfWPAPlugin
+{
+    public class WperfDataCooker : SourceDataCooker<CountingEvent, WperfSourceParser, string>
+    {
+        private readonly List<CountingEventWithRelativeTimestamp> countingEventWithRelativeTimestamps;
+
+        [DataOutput]
+        public IReadOnlyList<CountingEventWithRelativeTimestamp> CountingEventWithRelativeTimestamps { get; }
+
+        public WperfDataCooker()
+            : base(WperfPluginConstants.CookerPath)
+        {
+            countingEventWithRelativeTimestamps = new List<CountingEventWithRelativeTimestamp>();
+            CountingEventWithRelativeTimestamps =
+                new ReadOnlyCollection<CountingEventWithRelativeTimestamp>(
+                    countingEventWithRelativeTimestamps
+                );
+        }
+
+        public override string Description => "Adds relative timestamps to counting events";
+
+        // TODO : Check the needed keys for the events to send to this data cooker
+        public override ReadOnlyHashSet<string> DataKeys => throw new NotImplementedException();
+
+        public override DataProcessingResult CookDataElement(
+            CountingEvent data,
+            WperfSourceParser context,
+            CancellationToken cancellationToken
+        )
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WPAPlugin/WperfProcessingSourceWithSourceParser.cs
@@ -0,0 +1,103 @@
+﻿// BSD 3-Clause License
+//
+// Copyright (c) 2022, Arm Limited
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this
+//    list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its
+//    contributors may be used to endorse or promote products derived from
+//    this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+using Microsoft.Performance.SDK.Processing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WperfWPAPlugin
+{
+    [ProcessingSource(
+        "{05D8A372-EC56-43DE-83FF-02BDD9042727}",
+        "wperf Counting Data Source",
+        "Processes wperf counting trace files exported as JSON"
+    )]
+    [FileDataSource(".json", "JSON file output from wperf")]
+    public class SqlProcessingSourceWithSourceParser : ProcessingSource
+    {
+        private IApplicationEnvironment applicationEnvironment;
+
+        public override ProcessingSourceInfo GetAboutInfo()
+        {
+            return new ProcessingSourceInfo
+            {
+                CopyrightNotice = "Copyright 2021 Microsoft Corporation. All Rights Reserved.",
+                LicenseInfo = new LicenseInfo
+                {
+                    Name = "BSD 3-Clause License",
+                    Text = "Please see the link for the full license text.",
+                    Uri = "https://gitlab.com/Linaro/WindowsPerf/wpa-plugin/-/blob/main/LICENSE",
+                },
+                Owners = new[]
+                {
+                    new ContactInfo
+                    {
+                        // TODO: add appropriate contact information
+                    },
+                },
+                ProjectInfo = new ProjectInfo
+                {
+                    Uri = "https://gitlab.com/Linaro/WindowsPerf/wpa-plugin",
+                },
+            };
+        }
+
+        protected override ICustomDataProcessor CreateProcessorCore(
+            IEnumerable<IDataSource> dataSources,
+            IProcessorEnvironment processorEnvironment,
+            ProcessorOptions options
+        )
+        {
+            // TODO: Check if we eventually need to support multiple files
+            string[] filePathList = dataSources.Select(el => el.Uri.LocalPath).ToArray();
+            WperfSourceParser parser = new WperfSourceParser(filePathList);
+            return new WperfCustomDataProcessorWithSourceParser(
+                parser,
+                options,
+                applicationEnvironment,
+                processorEnvironment
+            );
+        }
+
+        protected override bool IsDataSourceSupportedCore(IDataSource source)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void SetApplicationEnvironmentCore(
+            IApplicationEnvironment applicationEnvironment
+        )
+        {
+            this.applicationEnvironment = applicationEnvironment;
+        }
+    }
+}
