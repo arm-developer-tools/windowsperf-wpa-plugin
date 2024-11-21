ce32b41ba3f47115ea8203a2f5c5164a9bc4d77b
Alaaeddine-chakroun <49242996+Alaaeddine-chakroun@users.noreply.github.com>
2024-11-21 16:56:53 +0100

    Fix file processing (#7)
    * fix: file processing
    
    * chore: cleanup
    
    * chore: bump version number
    
    * chore: bump version number

--- WPAPlugin/WperfProcessingSourceWithSourceParser.cs
+++ WPAPlugin/WperfProcessingSourceWithSourceParser.cs
@@ -31,7 +31,6 @@
 
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using Microsoft.Performance.SDK.Processing;
 using NJsonSchema;
 using WPAPlugin.Schemas;
@@ -50,6 +49,9 @@ namespace WPAPlugin
     public class WperfProcessingSourceWithSourceParser : ProcessingSource
     {
         private IApplicationEnvironment applicationEnvironment;
+
+        public WperfProcessingSourceWithSourceParser()
+            : base() { }
 
         /// <summary>
         /// Sets project information
@@ -92,13 +94,8 @@ namespace WPAPlugin
             ProcessorOptions options
         )
         {
-            WperfSourceParser parser = new WperfSourceParser(
-                timelinePathList.ToArray(),
-                countingPathList.ToArray()
-            );
+            WperfSourceParser parser = new WperfSourceParser(timelinePathList, countingPathList);
 
-            timelinePathList.Clear();
-            countingPathList.Clear();
             validationCache.Clear();
             return new WperfCustomDataProcessorWithSourceParser(
                 parser,
